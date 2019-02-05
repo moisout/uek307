c043653ab8a75aabc73056514dcefd60e5973f08
Maurice Oegerli <Maurice@HSG20150-N.Unisg.ch>
2019-02-05 08:35:33 +0100

    timing

--- k4/javascript/index.js
+++ k4/javascript/index.js
@@ -15,14 +15,43 @@ $(function () {
     $('.date-year').html(`© ${new Date().getFullYear()} Maurice Oegerli`);
 
     $('#seite1').load('components/page1.html');
-    
+
     $.ajax({
         type: "GET",
         url: "components/page2.json",
-        data: "data",
         dataType: "json",
-        success: function (data){
+        success: function (data) {
             $('#seite2').html(data.data);
         }
     });
+
+    $.ajax({
+        type: "GET",
+        url: "components/autos.json",
+        dataType: "json",
+        success: function (data) {
+            autoLaden(data);
+        }
+    });
+
+    async function autoLaden(autos) {
+        autos.forEach(element => {
+            await new Promise(resolve => $.ajax({
+                type: 'GET',
+                url: 'components/auto-template.html',
+                dataType: 'html',
+                success: function (data) {
+                    $('.auto-list').append(data);
+                    $('#auto_liste_template').prop('id', `auto_liste_${element.id}`);
+
+                    ['id', 'name', 'kraftstoff', 'farbe', 'bauart', 'tank']
+                    .forEach(item => {
+                        $(`#auto_liste_${element.id}`).find(`.${item}`).html(element[item]);
+                    });
+                    resolve(data);
+                }
+            }));
+        });
+    }
+
 });
